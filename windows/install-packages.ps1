d089ae4fd9b514b0fb6221018050bf3a569a88ee
raecheol.park <raecheol.park@seculetter.com>
2021-07-30 13:50:55 +0900

    first commit

---
+++ windows/install-packages.ps1
@@ -0,0 +1,16 @@
+choco install -y git
+choco install -y gitkraken
+choco install -y obsidian
+choco install -y typora
+choco install -y drawio
+
+choco install -y powertoys
+choco install -y microsoft-windows-terminal
+choco install -y vscode
+
+choco install -y google-drive-file-stream
+choco install -y googlechrome
+
+choco install -y intellijidea-ultimate
+
+choco install -y docker-desktop

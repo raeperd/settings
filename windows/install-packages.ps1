18bdca2437f31bcb5658e4961eb2fb9b934389f6
raeperd <raeperd117@gmail.com>
2021-08-03 22:42:06 +0900

    Add another packages

--- windows/install-packages.ps1
+++ windows/install-packages.ps1
@@ -1,8 +1,11 @@
 choco install -y git
 choco install -y gitkraken
+choco install -y drawio
+
 choco install -y obsidian
 choco install -y typora
-choco install -y drawio
+choco install -y authy-desktop
+choco install -y postman
 
 choco install -y powertoys
 choco install -y microsoft-windows-terminal
